08e12a334e6666d7f6df6569835a6d46f873c6fb
coolhacker13 <33355745+coolhacker13@users.noreply.github.com>
2019-02-10 01:00:01 +0300

    Добавьте файлы проекта.

---
+++ serverChat/Server.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace serverChat
+{
+    public static class Server
+    {
+        public static List<Client> Clients = new List<Client>();
+        public static void NewClient(Socket handle)
+        {
+            try
+            {
+                Client newClient = new Client(handle);
+                Clients.Add(newClient);
+                Console.WriteLine("New client connected: {0}", handle.RemoteEndPoint);
+            }
+            catch (Exception exp) { Console.WriteLine("Error with addNewClient: {0}.",exp.Message); }
+        }
+        public static void EndClient(Client client)
+        {
+            try
+            {
+                client.End();
+                Clients.Remove(client);
+                Console.WriteLine("User {0} has been disconnected.", client.UserName);
+            }
+            catch (Exception exp) { Console.WriteLine("Error with endClient: {0}.",exp.Message); }
+        }
+        public static void UpdateAllChats(string privateName)
+        {
+            if (string.IsNullOrWhiteSpace(privateName))
+            {
+                try
+                {
+                    int countUsers = Clients.Count;
+                    for (int i = 0; i < countUsers; i++)
+                    {
+                        Clients[i].UpdateChat();
+                    }
+                }
+                catch (Exception exp) { Console.WriteLine("Error with updateALLChats: {0}.", exp.Message); }
+            }
+            else
+            {
+                try
+                {
+                    int countUsers = Clients.Count;
+                    for (int i = 0; i < countUsers; i++)
+                    {
+                        if (Clients[i].UserName == privateName)
+                        Clients[i].UpdateChat();
+                    }
+                }
+                catch (Exception exp) { Console.WriteLine("Error with updateALLChats: {0}.", exp.Message); }
+            }
+        }
+        
+    }
+}
